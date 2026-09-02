6462e1829a90b090ea5d6f3c83a5325591d6d748
Billy Farrell <billyfarrell95@gmail.com>
2026-09-01 22:48:35 -0500

    save

--- universal-paperclips/main.js
+++ universal-paperclips/main.js
@@ -1,3 +1,12 @@
+// If you want to set a console.log notifier of when localStorage is saved:
+const origSetItem = localStorage.setItem.bind(localStorage);
+localStorage.setItem = function(key, value) {
+    origSetItem(key, value);
+    const now = new Date();
+    const hms = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    console.log(`Saved key: ${key} at ${hms}`);
+};
+
 // 1. In browser console of your active Universal Paperclips game run:
 copy(localStorage)
 
